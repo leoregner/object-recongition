374e75f81ff1d5e13f5ae4ad8254f1c368bcf981
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2019-02-15 21:38:47 +0100

    process engine enhanced

--- processengine/app.js
+++ processengine/app.js
@@ -3,6 +3,7 @@ angular.module('processEngine', [])
 .controller('bpmController', [ '$scope', function($scope)
 {
 	$scope.vars = {};
+    $scope.logs = [];
 	
 	$scope.process =
 	[
@@ -26,7 +27,7 @@ angular.module('processEngine', [])
 								{
 									"type": "script",
 									"name": "A2",
-									"script": "console.log('doing A2')"
+									"script": "console.log('doing A2: x = ' + this.x)"
 								}
 							]
 						}
@@ -78,29 +79,123 @@ angular.module('processEngine', [])
 		
 		else if(confirm('Do you want to execute this process now?'))
 		{
-			$scope.running = true;
-			
 			var errorHandler = function(x)
 			{
-				console.log(x);
+				console.log('Error', x);
 				alert('Error while executing process!');
 				$scope.$apply(function() { $scope.running = false });
 			};
 			
 			var successHandler = function()
 			{
-				$scope.$apply(function()
-				{
-					$scope.running = false;
-				});
+                console.log('Process execution finished');
+				$scope.$apply(function() { $scope.running = false });
 			};
 			
+            $scope.running = true;
+            console.log('Process execution started');
 			executeSequence($scope.process, $scope.vars).then(successHandler).catch(errorHandler);
 		}
 	};
 	
-	$scope.edit = function(task, parentBranch, index)
+	$scope.edit = function(task, parentBranch, index, $event)
 	{
-		alert(task.type + ' is the ' + index + 'th element in ' + parentBranch);// TODO
+        if($scope.running)
+            return;
+        
+        if(task && parentBranch && !index && !$event) // translation for function(branch, $event)
+        {
+            task = { type: 'sequence', branch: task };
+            $event = parentBranch;
+            parentBranch = undefined;
+        }
+        
+        // show it menu set its position
+        $scope.showMenu = { task: task, parentBranch: parentBranch, index: index };
+        $('#menu').css({ display: 'block', left: ($event.clientX + window.scrollX) + 'px', top: ($event.clientY + window.scrollY) + 'px' });
+        
+        $event.stopPropagation();
 	};
+    
+    $scope.deleteElement = function(element)
+    {
+        if(element.parentBranch && element.index)
+            element.parentBranch.splice(element.index, 1);
+        
+        else if(element.parentBranch && element.index == 0)
+            element.parentBranch.shift();
+        
+        $scope.hideMenu();
+    };
+    
+    $scope.insertAbove = function(element, type)
+    {
+        var insert = { type: type || 'script', name: 'new task' };
+        
+        if(element.parentBranch && element.index)
+            element.parentBranch.splice(element.index, 0, insert);
+        
+        else if(element.parentBranch && element.index == 0)
+            element.parentBranch.unshift(insert);
+        
+        else if(element.task.branch)
+            element.task.branch.push(insert);
+        
+        $scope.hideMenu();
+    };
+    
+    $scope.insertBelow = function(element, type)
+    {
+        var insert = { type: type || 'script', name: 'new task' };
+        
+        if(element.parentBranch && element.index)
+            element.parentBranch.splice(element.index + 1, 0, insert);
+        
+        else if(element.parentBranch && element.index == 0)
+            element.parentBranch.splice(1, 0, insert);
+        
+        else if(element.task.branch)
+            element.task.branch.push(insert);
+        
+        $scope.hideMenu();
+    };
+    
+    $scope.addBranch = function(element)
+    {
+        // TODO
+        
+        $scope.hideMenu();
+    };
+    
+    $scope.taskEditor = function(element)
+    {
+        // TODO
+        
+        $scope.hideMenu();
+    };
+    
+    $scope.hideMenu = function()
+    {
+        $scope.showMenu = false;
+        $('#menu').css({ display: 'none' });
+    };
+    
+    $scope.addVar = function()
+    {
+        var key = prompt('Insert variable name:');
+        if(key) $scope.vars[key] = '';
+    };
+    
+    $scope.deleteVar = function(key)
+    {
+        delete $scope.vars[key];
+    };
+    
+    console.log = function()
+    {
+        var msg = new Date().toLocaleString() + ':';
+        for(var arg of [].slice.call(arguments))
+            msg += ' ' + JSON.stringify(arg);
+        $scope.logs.push(msg);
+    };
 }]);
