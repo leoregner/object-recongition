2b11177daf1f7b4fc0600d5077d648730df6fd75
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2019-03-21 19:43:10 +0100

    3D reconstruction with opencv (omarpadierna)

--- omarpadierna/webservice/make_3d.sh
+++ omarpadierna/webservice/make_3d.sh
@@ -8,10 +8,12 @@ alias yellow="GREP_COLOR='1;33' grep --color=always ."
 echo "started rendering $1.obj" | yellow
 
 mv "in_$1" "../3DReconstruction/Reconstruction/reconstruct_this"
-
 cd "../3DReconstruction/Reconstruction"
-python3 disparity.py | red
 
+python3 disparity.py | red
 mv "reconstructed.ply" "../../webservice/$1.ply"
+
+python3 ply2obj.py reconstructed.ply | red
+mv "reconstructed.obj" "../../webservice/$1.obj"
 
 echo "done rendering $1.obj" | green
