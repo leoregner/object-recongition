4d3a84d872c7990bf8fb0803f0a1b68d8cc71faa
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2019-06-22 14:20:41 +0200

    finalized web service for real sense camera

---
+++ realsense/webservice/index.js
@@ -0,0 +1,17 @@
+const scan = require('./pointcloud.js'), express = require('express');
+
+let app = express();
+app.listen(8080);
+
+app.get('/obj', function(req, res)
+{
+    // store response instead of writing to std::out
+    let txt = ''; console.log = (line) => txt += line;
+
+    // use Intel RealSense camera and create point cloud
+    scan();
+
+    // send 3D model via web service
+    res.header('Content-Type', 'text/plain');
+    res.send(txt);
+});
--- realsense/webservice/pointcloud.js
+++ realsense/webservice/pointcloud.js
@@ -6,70 +6,81 @@
 
 const rs2 = require('node-librealsense'), fs = require('fs');
 
-const numberOfFrames = 100; // pcs
-const resolutionWidth = 848; // px
-const resolutionHeight = 480; // px
-
-const maxSideOffset = 0.20; //meters
-const legoStructureAngle = 45; // deg
-const minDistanceThreshold = 0.10; // meters
-const maxDistanceThreshold = 0.185; // meters
-
-const config = new rs2.Config();
-config.enableStream(rs2.stream.stream_color, -1, resolutionWidth, resolutionHeight, rs2.format.format_any, 30);
-config.enableStream(rs2.stream.stream_depth, -1, resolutionWidth, resolutionHeight, rs2.format.format_any, 30);
-
-const pipeline = new rs2.Pipeline();
-pipeline.start(config);
-
-for(let i = 0; i < numberOfFrames; ++i)
-    pipeline.waitForFrames();
-
-const pointcloud = new rs2.PointCloud();
-const frameSet = pipeline.waitForFrames();
-
-// get depth data
-const points = pointcloud.calculate(frameSet.depthFrame);
-
-// get color data and scale to depth coordinates
-const color = frameSet.colorFrame;
-pointcloud.mapTo(color); // FIXME
-
-if(points.vertices && points.textureCoordinates)
+module.exports = function(fileName)
 {
-    const vertices = [];
+    const numberOfFrames = 100; // pcs
+    const resolutionWidth = 848; // px
+    const resolutionHeight = 480; // px
 
-    /** @see https://github.com/IntelRealSense/librealsense/blob/master/src/archive.cpp#L48 */
-    for(let i = 0; i < points.size; ++i)
+    const maxSideOffset = 0.20; //meters
+    const legoStructureAngle = 45; // deg
+    const minDistanceThreshold = 0.10; // meters
+    const maxDistanceThreshold = 0.185; // meters
+
+    const config = new rs2.Config();
+    config.enableStream(rs2.stream.stream_color, -1, resolutionWidth, resolutionHeight, rs2.format.format_any, 30);
+    config.enableStream(rs2.stream.stream_depth, -1, resolutionWidth, resolutionHeight, rs2.format.format_any, 30);
+
+    const pipeline = new rs2.Pipeline();
+    pipeline.start(config);
+
+    for(let i = 0; i < numberOfFrames; ++i)
+        pipeline.waitForFrames();
+
+    const pointcloud = new rs2.PointCloud();
+    const frameSet = pipeline.waitForFrames();
+
+    // get depth data
+    const points = pointcloud.calculate(frameSet.depthFrame);
+
+    // get color data and scale to depth coordinates
+    const color = frameSet.colorFrame;
+    pointcloud.mapTo(color); // FIXME
+
+    if(points.vertices && points.textureCoordinates)
     {
-        const MIN_DISTANCE = 1e-6, vertice = { x: points.vertices[i * 3], y: -points.vertices[i * 3 + 1], z: -points.vertices[i * 3 + 2] };
-        vertice.floorDistance = mapDepthToFloor(legoStructureAngle, vertice.x, vertice.y, vertice.z);
+        const vertices = [];
 
-        if(Math.abs(vertice.x) >= MIN_DISTANCE || Math.abs(vertice.y) >= MIN_DISTANCE || Math.abs(vertice.z) >= MIN_DISTANCE)
-            if(vertice.x >= -maxSideOffset && vertice.x <= maxSideOffset && vertice.y >= -maxSideOffset && vertice.y <= maxSideOffset)
-                if(vertice.floorDistance > -maxDistanceThreshold && vertice.floorDistance < -minDistanceThreshold)
-                {
-                    let u = points.textureCoordinates[i * 2], v = points.textureCoordinates[i * 2 + 1], w = color.width, h = color.height;
-                    let x = Math.min(Math.max(parseInt(u * w + .5), 0), w - 1);
-                    let y = Math.min(Math.max(parseInt(v * h + .5), 0), h - 1);
-                    let idx = x * color.bytesPerPixel / 8 + y * color.strideInBytes;
+        /** @see https://github.com/IntelRealSense/librealsense/blob/master/src/archive.cpp#L48 */
+        for(let i = 0; i < points.size; ++i)
+        {
+            const MIN_DISTANCE = 1e-6, vertice = { x: points.vertices[i * 3], y: -points.vertices[i * 3 + 1], z: -points.vertices[i * 3 + 2] };
+            vertice.floorDistance = mapDepthToFloor(legoStructureAngle, vertice.x, vertice.y, vertice.z);
 
-                    vertice.red = color.data[idx];
-                    vertice.green = color.data[idx + 1];
-                    vertice.blue = color.data[idx + 2];
+            if(Math.abs(vertice.x) >= MIN_DISTANCE || Math.abs(vertice.y) >= MIN_DISTANCE || Math.abs(vertice.z) >= MIN_DISTANCE)
+                if(vertice.x >= -maxSideOffset && vertice.x <= maxSideOffset && vertice.y >= -maxSideOffset && vertice.y <= maxSideOffset)
+                    if(vertice.floorDistance > -maxDistanceThreshold && vertice.floorDistance < -minDistanceThreshold)
+                    {
+                        let u = points.textureCoordinates[i * 2], v = points.textureCoordinates[i * 2 + 1], w = color.width, h = color.height;
+                        let x = Math.min(Math.max(parseInt(u * w + .5), 0), w - 1);
+                        let y = Math.min(Math.max(parseInt(v * h + .5), 0), h - 1);
+                        let idx = x * color.bytesPerPixel / 8 + y * color.strideInBytes;
 
-                    vertices.push(vertice);
-                }
+                        vertice.red = color.data[idx];
+                        vertice.green = color.data[idx + 1];
+                        vertice.blue = color.data[idx + 2];
+
+                        vertices.push(vertice);
+                    }
+        }
+
+        // export to *.ply file
+        if(fileName && fileName.toLowerCase().substr(-4) == '.ply')
+            writeToPlyFile(fileName, vertices);
+
+        // export to *.obj file
+        else if(fileName && fileName.toLowerCase().substr(-4) == '.obj')
+            writeToObjFile('3d.obj', vertices);
+
+        // write to std::out in *.obj format
+        else writeToObjFile(null, vertices);
     }
 
-    //writeToPlyFile('3d.ply', vertices);
-    writeToObjFile('3d.obj', vertices);
+    pointcloud.destroy();
+    pipeline.stop();
+    pipeline.destroy();
+    rs2.cleanup();
 }
-
-pointcloud.destroy();
-pipeline.stop();
-pipeline.destroy();
-rs2.cleanup();
 
 function mapDepthToFloor(angle, x, y, depth)
 {
@@ -77,18 +88,14 @@ function mapDepthToFloor(angle, x, y, depth)
         return depth;
 
     if(angle == 45)
-    {
-        //const a = -.293723933, b = .6528543642, c = .3217031981, d = -.1332925141;
-        const a = .0307012186, b = .8044191429, c = .8287873915, d = 0;
-        return a * x + b * y + c * depth + d;
-    }
+        return .0307012186 * x + .8044191429 * y + .8287873915 * depth;
 
     throw 'depth mapping not defined for an angle of ' + angle + ' degrees';
 }
 
 function writeToPlyFile(fileName, vertices)
 {
-    const ply = fs.createWriteStream(fileName);
+    const ply = fileName ? fs.createWriteStream(fileName) : { write: console.log, end: ()=>{} };
     ply.write('ply\n');
     ply.write('\t\tformat ascii 1.0\n');
     ply.write('\t\telement vertex ' + vertices.length + '\n');
@@ -108,7 +115,7 @@ function writeToPlyFile(fileName, vertices)
 
 function writeToObjFile(fileName, vertices)
 {
-    const obj = fs.createWriteStream(fileName);
+    const obj = fileName ? fs.createWriteStream(fileName) : { write: console.log, end: ()=>{} };
     obj.write('# OBJ File\n');
     obj.write('# Vertices: ' + vertices.length + '\n# Faces: 0\n');
 
@@ -117,3 +124,7 @@ function writeToObjFile(fileName, vertices)
 
     obj.end();
 }
+
+// if started from command line, write to file
+if(process.argv.length > 2)
+    module.exports(process.argv[2]);
